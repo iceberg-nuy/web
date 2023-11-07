deebf7ff6d3705658d69d2f8fac0eb07813c6643
iceberg-nuy <121507277+iceberg-nuy@users.noreply.github.com>
2023-11-07 10:45:04 +0700

    Update datatable.js

--- datatable.js
+++ datatable.js
@@ -131,32 +131,32 @@ return DataTable;
 const html = `<div class="footer-basic">
 <footer>
 <div class="social">
-<a href="https://guruchian.blogspot.com/" target="_blank">
+<a href="https://www.google.co.th" target="_blank">
 <i class="gradient fa-brands fa-blogger-b"></i>
 </a>
-<a href="mailto:ph.wichian@gmail.com" target="_blank">
+<a href="mailto:theerapat_nuy@rtaf.mi.th" target="_blank">
 <i class="gradient fa-regular fa-envelope"></i>
 </a>
-<a href="https://github.com/semicon" target="_blank">
+<a href="https://www.google.co.th" target="_blank">
 <i class="gradient fa-brands fa-github-alt"></i>
 </a>
-<a href="https://facebook.com/ph.wichian" target="_blank">
+<a href="https://www.google.co.th" target="_blank">
 <i class="gradient fa-brands fa-facebook-f"></i>
 </a>
 </div>
 <ul class="gradient list-inline">
 <li class="list-inline-item" onclick="loadPage('home');" style="cursor:pointer;">Home</li>
-<li class="list-inline-item"><a href="https://guruchian.blogspot.com/p/blog-page.html"  target="_blank">About</a></li>
-<li class="list-inline-item"><a href="https://guruchian.blogspot.com/p/donate.html"  target="_blank">Donate</a></li>
-<li class="list-inline-item"><a href="https://web.facebook.com/groups/672966823753293/"  target="_blank">Terms</a></li>
-<li class="list-inline-item"><a href="https://guruchian.blogspot.com/p/license-conditions.html"  target="_blank">License</a></li>
+<li class="list-inline-item"><a href="https://www.google.co.th"  target="_blank">About</a></li>
+<li class="list-inline-item"><a href="https://www.google.co.th"  target="_blank">Donate</a></li>
+<li class="list-inline-item"><a href="https://www.google.co.th"  target="_blank">Terms</a></li>
+<li class="list-inline-item"><a href="https://www.google.co.th"  target="_blank">License</a></li>
 </ul>
 <p class="copyright">
 Site design  
 <img src='https://mirrors.creativecommons.org/presskit/icons/heart.red.png' width='25' height='25' alt='cc'>
 2023  by 
-<a href="https://guruchian.blogspot.com/p/blog-page.html" target="_blank" class="gradient"> KruChian</a><br>
-user contributions licensed under<span class="gradient"> CC BY-NC-SA</span>. rev 2023.05.20
+<a href="https://www.google.co.th" target="_blank" class="gradient"> พ.อ.อ.ธีรภัทร์ นุ้ยเล็ก</a><br>
+user contributions licensed under<span class="gradient"> CC BY-NC-SA</span>. rev 2023.11.07
 </p>
 </footer>
 </div>
